bd8c57b6cd8942533de9aa5852e803aff4aca09e
s4459561 <s4459561@935a8897-ea4c-3544-8dd0-fdfff3896a64>
2019-08-14 19:59:39 +0000

    update

--- trunk/ass1/bark.c
+++ trunk/ass1/bark.c
@@ -10,13 +10,17 @@ typedef struct {
     } Card;
 
 void initialize_board(int height, int width, char*** board);
+int load_saved_board(int height, int width, char*** board, FILE* deckFile);
 void display_empty_board(int height, int width, char*** board);
 void display_board(int height, int width, char*** board);
 void exit_with_error(int errorNum);
 int read_deck(FILE* deckFile, int cardAmount, char** deck);
 int initialize_game(char** deck, Card* p1Cards, Card* p2Cards, int n);
-void play_game(char*** board, char** deck, Card* p1Cards, Card* p2Cards, int n, int height, int width);
-void display_cards(Card* cards);
+void play_game(char*** board, char** deck, Card* p1Cards, Card* p2Cards, int n, int height, int width, char* p1Type, char* p2Type);
+void display_cards(Card* cards, int v, char* type);
+int first_move(int height, int width, Card* p1Cards, char*** board);
+void auto_first_move(int height, int width,Card* p1Cards, char*** board, int v);
+int move(int height, int width,int v, Card* p1Cards, Card* p2Cards, char*** board);
 void delete_board(int height, int width_1, char*** board);
 void free_board(int height, int width_1, char*** board);
 void free_deck(char** deck, int cardAmount);
@@ -41,14 +45,66 @@ int main(int argc, char** argv){
         if (saveFile == NULL){
             exit_with_error(4);
         }
-        char firstLine[8];
-        fgets(firstLine,8,saveFile);
-        char* deckName = calloc(83,sizeof(char));
-        fgets(deckName,83,saveFile);
-        fclose(saveFile);
+        int height, width, n, v;
+        char firstLine[81];
+        char secondLine[81];
+        char thirdLine[81];
+        char fourthLine[81];
+        fgets(firstLine,81,saveFile);
+        sscanf(firstLine,"%d %d %d %d", &width, &height, &n, &v);
+        if (height < 3 || height > 100){
+            exit_with_error(2);
+        }
+        if (width < 3 || width > 100){
+            exit_with_error(2);
+        }
+        if (v <1 || v > 2){
+            exit_with_error(4);
+        }
+        char* deckName = calloc(81,sizeof(char));
+        fgets(secondLine,81,saveFile);
+        sscanf(secondLine,"%s",deckName);
         FILE* deckFile = fopen(deckName,"r");
         if (deckFile == NULL){
+            free(deckName);
             exit_with_error(3);
+        }
+        char* N = calloc(81,sizeof(char));
+        fgets(N,81,deckFile); 
+        int cardAmount = atoi(N);
+        if (cardAmount < 1){
+            free(N);
+            exit_with_error(3);
+        }
+        fgets(thirdLine,81,saveFile);
+        fgets(fourthLine,81,saveFile);
+        //char savedBoard[height][width][2];
+        char*** board = calloc(height,sizeof(char**));
+        for (int i = 0; i < height; ++i){
+            board[i] = calloc(width,sizeof(char*));
+            for (int j = 0; j < width; ++j){
+                board[i][j]= calloc(2,sizeof(char));
+            }
+        }  
+        int error;
+        error = load_saved_board(height,width,board,saveFile);
+        if (error != 0){
+            free(N);
+            free_board(height, width, board);
+            exit_with_error(error);
+        }        
+        fclose(saveFile);
+        display_board(height, width, board);
+        char** deck = calloc(cardAmount,sizeof(char*));
+        for (int i = 0; i < cardAmount; ++i){
+        deck[i] = calloc(3,sizeof(char));
+        }
+        error = read_deck(deckFile,cardAmount,deck);
+        fclose(deckFile);
+        if (error != 0){
+            free(N);
+            free_deck(deck,cardAmount);
+            exit_with_error(error);
         }
     }
 
@@ -74,9 +130,13 @@ int main(int argc, char** argv){
         if (deckFile == NULL){ //can not open deckfile
             exit_with_error(3);
         }
-        char* N = calloc(83,sizeof(char));
-        fgets(N,83,deckFile); 
+        char* N = calloc(81,sizeof(char));
+        fgets(N,81,deckFile); 
         int cardAmount = atoi(N);
+        if (cardAmount < 1){
+            free(N);
+            exit_with_error(3);
+        }
         char** deck = calloc(cardAmount,sizeof(char*));
         for (int i = 0; i < cardAmount; ++i){
         deck[i] = calloc(3,sizeof(char));
@@ -88,11 +148,6 @@ int main(int argc, char** argv){
             free_deck(deck,cardAmount);
             exit_with_error(error);
         }
-        /*for (int i = 0; i < cardAmount; i++){
-            for (int j = 0; j < 3; j++){ 
-                putchar(deck[i][j]);  
-            }  
-        }*/
         //int width_1 = width + 1; // the width must be 1 larger than width
         char*** board = calloc(height,sizeof(char**));
         for (int i = 0; i < height; ++i){
@@ -107,12 +162,11 @@ int main(int argc, char** argv){
         int n = 0; //number of cards that have been drawn
         n = initialize_game(deck,p1Cards,p2Cards,n);
         display_board(height, width, board);
-        play_game(board,deck,p1Cards,p2Cards,n,height,width);
+        play_game(board,deck,p1Cards,p2Cards,n,height,width,p1Type,p2Type);
         free(p1Cards);
         free(p2Cards);
-        //initialize_board(height, width, board);
         //delete_board(height, width, board);
-        //free_board(height, width, board);
+        free_board(height, width, board);
     }
 }
 
@@ -130,6 +184,46 @@ void initialize_board(int height, int width, char*** board){ //create and save t
                 //board[i][j] = ".."; 
         }
     }
+}
+
+int load_saved_board(int height, int width, char*** board, FILE* saveFile){
+    int c;
+    char savedBoard[height][width][2];
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            for (size_t k = 0; k < 2; k++){
+                c = fgetc(saveFile);
+                if (c == '\n'){
+                    c = fgetc(saveFile);
+                }
+                if (c == '*'){
+                    savedBoard[i][j][k] = '.';
+                    putchar(savedBoard[i][j][k]);
+                }else{
+                    savedBoard[i][j][k] = c;
+                    putchar(savedBoard[i][j][k]);
+                }
+            }
+        }
+        printf("\n");
+    }
+    c = fgetc(saveFile);
+    if (c != '\n'&& c != EOF){
+        return 4;
+    }
+    printf("\n");
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            if (savedBoard[i][j][0] != '.'){
+                board[i][j][0] = (int)(savedBoard[i][j][0]-'0');
+                board[i][j][1] = savedBoard[i][j][1];
+            }else{
+                board[i][j][0] = savedBoard[i][j][0];
+                board[i][j][1] = savedBoard[i][j][1];
+            }
+        }
+    }
+    return 0;
 }
 
 void display_empty_board(int height, int width, char*** board){ //display the board
@@ -174,16 +268,10 @@ int read_deck(FILE* deckFile, int cardAmount, char** deck){
             }
         }  
     }
-    // int d;
     c = fgetc(deckFile);
     if (c != EOF){ // check if the number of card is more than N
         return 3;
     }
-    /* for (int i = 0; i < cardAmount; i++){
-        for (int j = 0; j < 3; j++){ 
-            putchar(deck[i][j]);  
-        }  
-    }*/
     return 0;   
 }
 
@@ -203,74 +291,176 @@ int initialize_game(char** deck, Card* p1Cards, Card* p2Cards, int n){
     return n;
 }
 
-void play_game(char*** board, char** deck, Card* p1Cards, Card* p2Cards, int n, int height, int width){
+void play_game(char*** board, char** deck, Card* p1Cards, Card* p2Cards, int n, int height, int width, char* p1Type, char* p2Type){
+    int v = 1; // indicating which player should have their turn now 
+    int error; 
     p1Cards[5].num = (int)(deck[n][0]-'0');
     p1Cards[5].suit = deck[n][1];
-    display_cards(p1Cards);
+    n = n + 1;
+    display_cards(p1Cards,v,p1Type);
+    if (!strcmp(p1Type,"h")){
+        error = first_move(height,width,p1Cards,board);
+        if (error != 0){
+            exit_with_error(error);
+        }  
+    }else{
+        auto_first_move(height,width,p1Cards,board,v);
+    }
+    v = 2;
+    display_board(height, width, board);
+    p2Cards[5].num = (int)(deck[n][0]-'0');
+    p2Cards[5].suit = deck[n][1];
+    n = n + 1;
+    display_cards(p2Cards,v,p2Type);
+    error = move(height,width,v,p1Cards,p2Cards,board);
+    if (error != 0){
+        exit_with_error(error);
+    } 
+    v = 1;
+    display_board(height, width, board);
+}
+
+void display_cards(Card* cards, int v, char* type){
+    if (!strcmp(type,"h")){
+        switch (v){
+        case 1:
+            printf("Hand(1): ");
+            break;
+        case 2:
+            printf("Hand(2): ");
+        default:
+            ;
+        }
+        for (int i = 0; i < 6; i++){
+            printf("%d%c",cards[i].num,cards[i].suit);
+            if (i == 5){
+                printf("\n");
+            }else{
+                printf(" ");
+            }
+        }  
+    }else{
+        printf("Hand: ");
+        for (int i = 0; i < 6; i++){
+            printf("%d%c",cards[i].num,cards[i].suit);
+            if (i == 5){
+                printf("\n");
+            }else{
+                printf(" ");
+            }
+        }  
+    }
+}
+
+int first_move(int height, int width, Card* p1Cards, char*** board){
     int card,c,r;
-    char card1,c1,r1;
-    char str[6];
+    char str[81];
     char* i;
     int j = 0;
     while (j == 0){
         printf("Move? ");
-        i = fgets(str,6,stdin);
+        i = fgets(str,81,stdin);
         if (str[0] == '\n'){
-            i = fgets(str,6,stdin);
+            i = fgets(str,81,stdin);
         }
         if (i == NULL){
-            exit_with_error(7);
+            return 7;
         }
-        card1 = str[0];
-        c1 = str[2];
-        r1 = str[4];
-        card = (int)(card1-'0');
-        c = (int)(c1-'0');
-        r = (int)(r1-'0');
-        //printf("%d\n",c);
+        sscanf(str,"%d %d %d", &card, &c, &r);
         if(card <= 0 || card > 6){
             j = 0;
-            //printf("1");
             continue;
         }
         if(c <= 0 || c > width){
             j = 0;
-            //printf("2");
             continue;
         }
         if(r <= 0 || r > height){
             j = 0;
-            //printf("3");
             continue;
         }
+        board[r-1][c-1][0] = p1Cards[card-1].num;
+        board[r-1][c-1][1] = p1Cards[card-1].suit;
         j = 1;
     }
-    if(c <= width && c > 0){
+    /*if(c <= width && c > 0){
         if (r <= height && r >0){
             board[r-1][c-1][0] = p1Cards[card-1].num;
             board[r-1][c-1][1] = p1Cards[card-1].suit;
         }
-    }
-    display_board(height, width, board);
+    }*/
+    return 0;
 }
 
-void display_cards(Card* cards){
-    printf("Hand(1): ");
-    for (int i = 0; i < 6; i++){
-        printf("%d%c",cards[i].num,cards[i].suit);
-        if (i == 5){
-            printf("\n");
-        }else{
-            printf(" ");
+void auto_first_move(int height, int width,Card* p1Cards, char*** board, int v){
+    int x = (height + 1)/2;
+    int y = (width + 1)/2;
+    board[x-1][y-1][0] = p1Cards[0].num;
+    board[x-1][y-1][1] = p1Cards[0].suit;
+    printf("Player %d plays %d%c in column %d row %d\n",v,p1Cards[0].num,p1Cards[0].suit,x,y);
+}
+
+int move(int height, int width,int v, Card* p1Cards, Card* p2Cards, char*** board){
+    int card,c,r;
+    char str[81];
+    char* i;
+    int j = 0;
+    int a = -1;
+    while (j == 0){
+        printf("Move? ");
+        i = fgets(str,81,stdin);
+        if (str[0] == '\n'){
+            i = fgets(str,81,stdin);
         }
-        //printf(" ");
+        if (i == NULL){
+            return 7;
+        }
+        sscanf(str,"%d %d %d", &card, &c, &r);
+        if(card <= 0 || card > 6){
+            j = 0;
+            continue;
+        }
+        if(c <= 0 || c > width){
+            j = 0;
+            continue;
+        }
+        if(r <= 0 || r > height){
+            j = 0;
+            continue;
+        }
+        if (board[r-1][c-1][0] != '.'){
+            j = 0;
+            continue;
+        }
+        int x = r+a;
+        int y = c;
+        if (x > height ){
+            x = x - height;
+        }
+        if (x < 1){
+            x = x + height;
+        }
+        if (board[x-1][y-1][0] == '.' && board[x-1][y-1][1] == '.'){
+            j = 0;
+            continue;
+        }
+        //board[r-1][c-1][0] = p1Cards[card-1].num;
+        //board[r-1][c-1][1] = p1Cards[card-1].suit;
+        j = 1;  
     }
-    //printf("\n");
+    if (v == 1){
+        board[r-1][c-1][0] = p1Cards[card-1].num;
+        board[r-1][c-1][1] = p1Cards[card-1].suit;
+    }
+    if (v == 2){
+        board[r-1][c-1][0] = p2Cards[card-1].num;
+        board[r-1][c-1][1] = p2Cards[card-1].suit;
+    }
+    return 0;
 }
 
 void exit_with_error(int errorNum){
-    switch (errorNum)
-    {
+    switch (errorNum){
     case 1:
         fprintf(stderr, "Usage: bark savefile p1type p2type\n"); 
         fprintf(stderr,"bark deck width height p1type p2type\n");
